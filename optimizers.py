2ad093e45944945c842a9c97714f652a18a65889
Jianting Liu <137365338+TJLJT0929@users.noreply.github.com>
2025-03-20 21:20:23 +0800

    Update optimizers.py

--- optimizers.py
+++ optimizers.py
@@ -1,37 +1,5 @@
 import torch
 
-def adam_update(model, G_t_hat, m_t, v_t, v_hat, t, lr, betas, lambda_, eps, device, q):
-    beta1 = betas[0] * lambda_ ** (t-1)
-    beta2 = betas[1]
-    alpha_t = lr / t**0.5
-    with torch.no_grad():
-        for i, param in enumerate(model.parameters()):
-            m_t[i] = beta1 * m_t[i] + (1 - beta1) * G_t_hat[i]
-            v_t[i] = beta2 * v_t[i] + (1 - beta2) * (G_t_hat[i] ** 2)
-            
-            m_hat_t = m_t[i] / (1- beta1 ** t)
-            v_hat_t = v_t[i] / (1 - beta2 ** t)
-            
-            update_step = alpha_t * m_hat_t / (torch.sqrt(v_hat_t) + eps)
-            param.data -= update_step.to(device)
-
-
-def amsgrad_update(model, G_t_hat, m_t, v_t, v_hat, t, lr, betas, lambda_, eps, device, q):
-    beta1 = betas[0] * lambda_ ** (t-1)
-    beta2 = betas[1]
-    alpha_t = lr / t**0.5
-    with torch.no_grad():
-        for i, param in enumerate(model.parameters()):
-            m_t[i] = beta1 * m_t[i] + (1 - beta1) * G_t_hat[i]
-            v_t[i] = beta2 * v_t[i] + (1 - beta2) * (G_t_hat[i] ** 2)
-            v_hat[i] = torch.max(v_hat[i], v_t[i])
-
-            m_hat_t = m_t[i] / (1- beta1 ** t)
-            v_hat_t = v_hat[i] / (1 - beta2 ** t)
-            
-            update_step = alpha_t * m_hat_t / (torch.sqrt(v_hat_t) + eps)
-            param.data -= update_step.to(device)
-
 
 def adam_corrected_update(model, G_t_hat, m_t, v_t, v_hat, t, lr, betas, lambda_, eps, device, q):
     beta1 = betas[0] * lambda_ ** (t-1)
